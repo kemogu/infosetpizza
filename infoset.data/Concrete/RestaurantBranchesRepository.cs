12592bb11c71add64526c271eb97dbf180e5413d
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2024-06-06 10:03:49 +0300

    helpers layer added

--- infoset.data/Concrete/RestaurantBranchesRepository.cs
+++ infoset.data/Concrete/RestaurantBranchesRepository.cs
@@ -1,17 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using infoset.data.Abstract;
 using infoset.entity;
+using infoset.helpers;
+using Microsoft.EntityFrameworkCore;
 
 namespace infoset.data.Concrete
 {
     public class RestaurantBranchesRepository : IRestaurantBranchesRepository
     {
+        public readonly AppDbContext _dbContext;
+        public readonly DbSet<RestaurantBranch> _dbSet;
+        public RestaurantBranchesRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<RestaurantBranch>();
+        }
         public IQueryable<RestaurantBranch> GetNearstRestaurantBranches(double latitude, double longitude, double maxDistance, int takeNumber)
         {
-            throw new NotImplementedException();
+            return _dbSet.Where(rb => DistanceHelper.CalculateDistance(latitude, longitude, rb.Latitude, rb.Longitude) <= maxDistance)
+            .OrderBy(rb => DistanceHelper.CalculateDistance(latitude, longitude, rb.Latitude, rb.Longitude))
+            .Take(takeNumber)
+            .ToList();
         }
     }
 }
